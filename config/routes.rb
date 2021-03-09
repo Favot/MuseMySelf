39a9a754cc378af3e7d9b154e4aad2077dcea996
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-03-09 12:43:06 +0100

    init of routes config, to be checked

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :journeys, only: %i[index show] do
+    resources :user_journeys, only: %i[create]
+
+  end
+  resources :user_journeys, only: %i[index show] do
+    patch :complete
+  end
+  resources :user_journey_contents, only: %i[show edit update]
 end
