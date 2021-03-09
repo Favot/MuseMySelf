3be5ebbc471ee24204962d907febc010df55ae52
Favot <favot.corentin.b@gmail.com>
2021-03-09 15:26:54 +0100

    add validation ligth for to have a safe seed

--- app/models/journey_content.rb
+++ app/models/journey_content.rb
@@ -1,4 +1,12 @@
 class JourneyContent < ApplicationRecord
   belongs_to :content
   belongs_to :journey
+
+  # validation really ligth only to check the seed are good
+
+  validates :position, presence: true
+
+  validates :content_id, presence: true
+
+  validates :journey_id, presence: true
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ApplicationRecord
   has_many :journeys
+
+  # validation really ligth only to check the seed are good
+
+  validates :name, presence: true
 end
