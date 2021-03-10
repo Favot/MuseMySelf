d057a2327277f54dd09579243e4b26256b05235b
Favot <favot.corentin.b@gmail.com>
2021-03-10 09:29:39 +0100

    correction index controller and standby

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,12 +1,12 @@
 class JourneysController < ApplicationController
-  def index
-    @journeys = Journey.all
+  # def index
+  #   @journeys = Journey.all
 
-    if params[:search].present?
-      @journeys = @journeys.where('name ILIKE ?', "%#{
-            params[:search]['query']}%")
-    end
+  #   if params[:search].present?
+  #     @journeys = @journeys.where('name ILIKE ?', "%#{
+  #           params[:search]}%")
+  #   end
 
-    @journeys = @journeys.where(topic_id: params[:topic_id]) if params[:topic_id].present?
-  end
+  #   @journeys = @journeys.where(topic_id: params[:topic_id]) if params[:topic_id].present?
+  # end
 end
