b144abc184a76256cb12276befec0b69c004363c
Favot <favot.corentin.b@gmail.com>
2021-03-10 14:15:50 +0100

    creation of the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @topic = Topic.all
+    @topics = Topic.all
+
+    # top 3 journey
+    @popular_journeys = Journey.all
+
+    # last 3 journey
+    @last_journeys = Journey.all.last(3).reverse
+
+    # 3 randoms journey
+    @random_journeys = Journey.all.sample(3)
+    # raise
   end
 end
