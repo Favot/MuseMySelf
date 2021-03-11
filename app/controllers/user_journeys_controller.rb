a06325c515587ad91e4ea1d68976268c3a7ba089
Favot <favot.corentin.b@gmail.com>
2021-03-11 15:05:35 +0100

    remove params at ligne 3

--- app/controllers/user_journeys_controller.rb
+++ app/controllers/user_journeys_controller.rb
@@ -1,10 +1,10 @@
 class UserJourneysController < ApplicationController
   def create
-    @user_journey = UserJourney.new(user_journey_params)
+    @user_journey = UserJourney.new
     @journey = Journey.find(params[:journey_id])
     @user_journey.journey = @journey
     @user_journey.user = current_user
-    raise
+
     if @user_journey.save!
       flash[:notice] = 'Vous avez etez enregistrer sur le parcours!'
       redirect_to journey_path(params[:journey_id])
