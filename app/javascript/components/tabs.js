64614ab9a6bcfdec353f98047f1f264d4c588472
lise henry <lise.henry@essca.eu>
2021-03-13 15:19:42 +0100

    js function for tabs done (showing corresponding content on click)

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,24 +1,24 @@
-// Adding .active on clicked tab for user_journeys#index
+// Show tabs for user_journeys#index
 
 const displayTab = () => {
-  // console.log(event.target)
+  const tabs = document.querySelector(".tabs");
+  const tabButtons = document.querySelectorAll(".tab-button");
+  const tabContents = document.querySelectorAll(".tab-content");
+  const id = event.target.dataset.id;
 
-  const tabs = document.querySelector("#myTabs");
-  const tabButton = document.querySelectorAll(".tab-button");
-  const contents = document.querySelectorAll(".tab-content");
-
-  const id = event.target.id;
   if (id) {
-    tabButton.forEach((button) => {
+    tabButtons.forEach((button) => {
       button.classList.remove("btn-active");
     });
     event.target.classList.add("btn-active");
 
-    contents.forEach((content) => {
-      content.classList.remove("active");
+    tabContents.forEach((content) => {
+      content.classList.remove("show");
+      if (content.id == id) {
+        const hiddenContent = document.getElementById(id);
+        hiddenContent.classList.add("show");
+      }
     });
-    const hiddenContent = document.getElementById(id);
-    hiddenContent.classList.add("active");
   }
 };
 
