d86423f8fd490f4ab0f05142c0166b19af916d4c
lise henry <lise.henry@essca.eu>
2021-03-13 09:46:29 +0100

    working on js for tabs

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,13 +1,25 @@
 // Adding .active on clicked tab for user_journeys#index
 
 const displayTab = () => {
-  console.log(event.target)
+  // console.log(event.target)
 
-  const tabs = document.querySelector('#myTabs');
-  const tabButton = document.querySelectorAll()
-  const contents =
+  const tabs = document.querySelector("#myTabs");
+  const tabButton = document.querySelectorAll(".tab-button");
+  const contents = document.querySelectorAll(".tab-content");
 
+  const id = event.target.id;
+  if (id) {
+    tabButton.forEach((button) => {
+      button.classList.remove("btn-active");
+    });
+    event.target.classList.add("btn-active");
 
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    const hiddenContent = document.getElementById(id);
+    hiddenContent.classList.add("active");
+  }
 };
 
 export {displayTab}
