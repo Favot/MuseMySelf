d5956706bbb18df0e41dc267baeb37a44b7a9140
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-03-15 17:01:06 +0100

    init user journey content show and update

--- app/controllers/user_journey_contents_controller.rb
+++ app/controllers/user_journey_contents_controller.rb
@@ -1,7 +1,22 @@
 class UserJourneyContentsController < ApplicationController
   def show
-    @user_journey_contents = UserJourneyContent.find(params[:id])
-    @content = @user_journey_contents.content
-    
+    @user_journey_content = UserJourneyContent.find(params[:id])
+    @content = @user_journey_content.content
+  end
+
+  def update
+    @user_journey_content = UserJourneyContent.find(params[:id])
+
+    if @user_journey_content.update(user_journey_content_params)
+      redirect_to user_journey_content_path(@user_journey_content)
+    else
+      render :update
+    end
+  end
+
+  private
+
+  def user_journey_content_params
+    params.require(:user_journey_content).permit(:rating, :review, :completed)
   end
 end
