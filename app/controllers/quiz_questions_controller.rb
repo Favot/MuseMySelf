20d3767f15f3843a0116f5a7314ee7930ec1288e
lise henry <lise.henry@essca.eu>
2021-03-15 15:19:45 +0100

    created QuizQuestions controller, method index, view

---
+++ app/controllers/quiz_questions_controller.rb
@@ -0,0 +1,6 @@
+class QuizQuestionsController < ApplicationController
+  before_action :authenticate_user!, only: %i[index]
+
+  def index
+  end
+end
---
+++ test/controllers/quiz_questions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuizQuestionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
