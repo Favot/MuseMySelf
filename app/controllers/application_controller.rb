382c496969a1754fe09f21b422bf18be60e684ab
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-03-16 15:25:54 +0100

    init of SEO stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
